3593367e81b65e8ec170bdb604ae30122ccd788f
Neel1054 <2020nupatel@gmail.com>
2024-06-18 13:41:47 -0500

    add file

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,32 @@
+// TODO: Create 4-String variables to introduce and develop your story.
+// YOUR CODE GOES HERE
+let greetings = "Hidden deep within the Berry forest, there was a hidden village";
+let twist = "in this village everyone had powers!";
+let hero = "John";
+let villan = "Jerry";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let year = "2500";
+let yearsTaken = "10";
+let deaths = "100";
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let superPowers = ['fire control', 'superspeed', 'lightning control'];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let johnWins = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greetings + ". However, it was no ordinary village, as " + twist);
+console.log("In this village, the strongest people were " + hero + " and " + villan);
+console.log(hero + " had " + superPowers[0] + " and " + villan + " had " + superPowers[2]);
+console.log("They were very different, as " + hero + " was kind, while " + villan + " wanted to be the only person with powers.");
+console.log("In the year " + year + ", " + villan + " decided that he would attack the village.");
+console.log(hero + " decided to protect the village, and so they fought for " + yearsTaken + " years.");
+console.log("In this time, there were " + deaths + " deaths.");
+console.log("In the end, smoke filled the air, and no one could see who had won and lost. Everyone in the village was praying for John to win");
+console.log("Their wishes became " + johnWins + " and everyone in the village lived happily ever after.");
